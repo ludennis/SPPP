3e36623792ae60ac39781b03069252736ef4df88
Dennis <ludennis0606@gmail.com>
2017-11-27 20:50:41 -0800

    Added features 	- hold power note added with each NoteOn 		- HOLD_POWER_START_MSEC 		- HOOLD_POWER 	- automatically scale NoteOn power 		- KEY_SCALE

--- parser.py
+++ parser.py
@@ -9,12 +9,23 @@ import re
 #CONSTANTS to tweak
 TAIL_GAP_MSEC = 250
 MIN_DURATION = 80
+HOLD_POWER_START_MSEC = 50
+HOLD_POWER = 35
 COM_SERIAL = 'COM3'
 
+#KEY_SCALE will multiply the value set to the corresponding key
+KEY_SCALE = [0, 
+			 1,1,1,1,1,1,1,1,1,1, #keys 1-10
+			 1,1,1,1,1,1,1,1,1,1, #keys 11-20
+			 1,1,1,1,1,1,1,1,1,1, #keys 21-30
+			 1,1,1,1,1,1,1,1,1,1, #keys 31-40
+			 1,1,1,1,1,1,1,1,1,1, #keys 41-50
+			 1,1,1,1,1,1,1,1,1,1, #keys 51-60
+			 1,1,1,1,1,1,1,1,1,1, #keys 61-70
+			 1,1,1,1]             #keys 71-74
 
 parser = argparse.ArgumentParser(description='Parses Midi Text file into Arduino Commands')
 parser.add_argument('input_file', metavar='input', type=str, nargs='?', help='the name of the input midi text file')
-
 args = parser.parse_args()
 
 read_file = open(args.input_file, 'r')
@@ -27,7 +38,7 @@ for line in read_file:
 		if match.group('action') == 'NoteOn':
 			match = re.match(r' [a-zA-Z]+: (?P<note>[0-9]+) [a-zA-Z]+: (?P<vol>[0-9]+) [a-zA-Z]+: (?P<dur>[0-9]+)',match.group('params'))
 			if match:
-				l.append([time_in_msec,int(match.group('note')),int(match.group('vol'))])
+				l.append([time_in_msec,int(match.group('note')),int(match.group('vol')*KEY_SCALE[int(match.group('note'))])])
 		elif match.group('action') == 'NoteOff':
 			match = re.match(r' [a-zA-Z]+: (?P<note>[0-9]+)',match.group('params'))
 			if match:
@@ -50,16 +61,22 @@ while i < len(l) - 1:
 			l[i][0] = l[i-1][0] + MIN_DURATION
 		else:
 			l[i][0] = l[i+1][0] - TAIL_GAP_MSEC
-		print('changed ' + str(l[i]) + '\n')
+		print 'changed ' + str(l[i]) + '\n'
+	elif l[i][1] != 150 and l[i][2] != 0 and l[i][2] != HOLD_POWER and l[i+1][0] - l[i][0] > MIN_DURATION:
+		print 'checking to add power hold: ' + str(l[i]) + ' next action ' + str(l[i+1]) 
+		l.insert(i+1,[l[i][0] + HOLD_POWER_START_MSEC,l[i][1],HOLD_POWER])
+		print 'added ' + str(l[i+1]) + '\n'
 	i+=1
 
-l.sort()
 
+#sort according to timestamp and change to delta t
+l.sort()
 i = len(l) - 1
 while i > 1:
 	l[i][0] = l[i][0] - l[i-1][0]
 	i-=1
 
+#write files
 write_file = open(args.input_file[:len(args.input_file)-4] + '.py', 'w')
 write_file.write('import serial\n')
 write_file.write('import time\n')
