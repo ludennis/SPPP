10dd15d7eb10f90563dc5d4ad9cca2b36aa73965
Dennis <ludennis0606@gmail.com>
2017-11-27 20:17:06 -0800

    First commit 	- updated README 	- parser.py

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # SPPP
 Self-playing piano parser
+
+Instructions
+1. copy and paste parser.py into the directory that contains your midi file
+2. use "dir" and "cd" to traverse into the directory
+2. call python parser.py [input_file_name] (ex C:\python27\python.exe parser.py what-made-you-die.txt)
+3. a python file named input_file_name.py will be created
---
+++ parser.py
@@ -0,0 +1,73 @@
+import argparse
+import re
+
+#TODO: Hold power after each NoteOn (for cooling off)
+#TODO: Each key on piano to tweak:
+#      - multiplier
+#      - offset
+
+#CONSTANTS to tweak
+TAIL_GAP_MSEC = 250
+MIN_DURATION = 80
+COM_SERIAL = 'COM3'
+
+
+parser = argparse.ArgumentParser(description='Parses Midi Text file into Arduino Commands')
+parser.add_argument('input_file', metavar='input', type=str, nargs='?', help='the name of the input midi text file')
+
+args = parser.parse_args()
+
+read_file = open(args.input_file, 'r')
+l = []
+
+for line in read_file:
+	match = re.match(r'[0-9]+,Min:Sec:Msec=(?P<min>[0-9]+):(?P<sec>[0-9]+):(?P<msec>[0-9]+),(?P<action>[a-zA-Z]+) chan: 1(?P<params>( [a-zA-Z]+: [0-9]+)+)', line)
+	if match:
+		time_in_msec = int(match.group('min')) * 60000 + int(match.group('sec')) * 1000 + int(match.group('msec'))
+		if match.group('action') == 'NoteOn':
+			match = re.match(r' [a-zA-Z]+: (?P<note>[0-9]+) [a-zA-Z]+: (?P<vol>[0-9]+) [a-zA-Z]+: (?P<dur>[0-9]+)',match.group('params'))
+			if match:
+				l.append([time_in_msec,int(match.group('note')),int(match.group('vol'))])
+		elif match.group('action') == 'NoteOff':
+			match = re.match(r' [a-zA-Z]+: (?P<note>[0-9]+)',match.group('params'))
+			if match:
+				l.append([time_in_msec,int(match.group('note')),int(0)])
+		elif match.group('action') == 'Sustain':
+			match = re.match(r' [a-zA-Z]+: (?P<val>[0-9]+)',match.group('params'))
+			l.append([time_in_msec,int(150),int(match.group('val'))])
+
+
+#sort the list according to note and then timestamp
+l.sort(key=lambda x: (x[1],x[0]))
+
+#cut tail of note when it is immediately played again by 50ms
+#if diff(timestamp(NoteOff) - timestamp(NoteOn) < 50) then timestamp(NoteOff) - 50ms
+i = 0
+while i < len(l) - 1:
+	if l[i][1] != 150 and l[i][2] == 0 and l[i][1] == l[i+1][1] and l[i+1][0] - l[i][0] < TAIL_GAP_MSEC:
+		print 'checking: NoteOn' + str(l[i-1]) + ' , NoteOff' + str(l[i]) + ' , next NoteOn' + str(l[i+1])
+		if l[i+1][0] - TAIL_GAP_MSEC - l[i-1][0] < MIN_DURATION:
+			l[i][0] = l[i-1][0] + MIN_DURATION
+		else:
+			l[i][0] = l[i+1][0] - TAIL_GAP_MSEC
+		print('changed ' + str(l[i]) + '\n')
+	i+=1
+
+l.sort()
+
+i = len(l) - 1
+while i > 1:
+	l[i][0] = l[i][0] - l[i-1][0]
+	i-=1
+
+write_file = open(args.input_file[:len(args.input_file)-4] + '.py', 'w')
+write_file.write('import serial\n')
+write_file.write('import time\n')
+write_file.write('ser = serial.Serial(\'' + COM_SERIAL + '\', 115200, timeout=5)\n')
+write_file.write('time.sleep(1)\n\n')
+
+for i in l:
+	write_file.write('ser.write(\'<' + str(i[0]) + ',' + str(i[1]) + ',' + str(i[2]) + '>\')\n')
+	write_file.write('ser.readline()\n')
+
+print '\'' + args.input_file[:len(args.input_file)-4] + '.py\' has been created'
