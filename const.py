68dac9a474edcc8b399d5e8e9e26ec3c302535bc
Dennis <ludennis0606@gmail.com>
2017-12-17 21:44:08 -0800

    Update const.py
    Updated const.py with Evan's values

--- const.py
+++ const.py
@@ -8,14 +8,24 @@
 # SUSTAIN_NOTE => the note that a sustain will be used 
 # NOTE_SCALE => will multiply the value set to the corresponding note
 
-TAIL_GAP_MSEC = 150
-MIN_NOTE_DUR = 200
-HOLD_DELAY_POWER_START_MSEC = 170
-HOLD_DELAY_POWER = 3
+# TAIL_GAP_MSEC = 30
+# MIN_NOTE_DUR = 35
+# HOLD_DELAY_POWER_START_MSEC = 90
+# HOLD_DELAY_POWER = 75
+# COM_SERIAL = 'COM11'
+# SUSTAIN_NOTE = 150
+# TARGET_MAX = 152
+# TARGET_MIN = 135
+
+
+TAIL_GAP_MSEC = 50
+MIN_NOTE_DUR = 30
+HOLD_DELAY_POWER_START_MSEC = 90
+HOLD_DELAY_POWER = 75
 COM_SERIAL = 'COM11'
 SUSTAIN_NOTE = 150
-TARGET_MAX = 105
-TARGET_MIN = 180
+TARGET_MAX = 155
+TARGET_MIN = 138
 
 NOTE_SCALE = [0, 
 			 0,0,0,0,0,0,0,0,0,0,     #ignore
@@ -45,11 +55,11 @@ NOTE_OFFSET = [0,
 			 
 			 
 			   0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, #Octave 1 notes 24-35
-			   0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, #Octave 2 notes 36-47
-			   0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, #Octave 3 notes 48-59
+			  -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, #Octave 2 notes 36-47
+			  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, #Octave 3 notes 48-59
 			   0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, #Octave 4 notes 60-71
 			   1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1, #Octave 5 notes 72-83
-			   2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2, #Octave 6 notes 84-96
+			   5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5, #Octave 6 notes 84-96
 			 
 			 
 			 0,0,0,0,                 #ignore, this is up to 100
